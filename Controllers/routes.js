e45b11a04a94c660c710e52e9e315816b9662241
timdean27 <timdean27@yahoo.com>
2022-05-18 19:54:20 -0400

    made starter(get/id post put and delete) routes

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -1,3 +1,39 @@
 export const getRoutes = (req, res) =>{
     res.send("Back end port 4000")
 };
+
+
+// export const getIDRoutes = async (req, res) =>{
+//     try {
+//         res.json(await info.find({}));
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// };
+
+// export const postRoutes = async (req, res) =>{
+//     try {
+//         res.json(await info.create(req.body))
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// };
+
+
+// export const putRoutes = async (req, res) =>{
+//     try {
+//         res.json(await info.findByIdAndUpdate(req.params.id, req.body))
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// };
+
+// export const deleteRoutes = async (req, res) =>{
+//     try {
+//         res.json(await info.findByIdAndRemove(req.params.id))
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// };
+
+
