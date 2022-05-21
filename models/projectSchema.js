c0590752409ac58d0de1a26daec7231996a7898a
timdean27 <timdean27@yahoo.com>
2022-05-21 11:05:43 -0400

    updated schema passing more useful seed data

--- models/projectSchema.js
+++ models/projectSchema.js
@@ -1,13 +1,41 @@
 const mongoose = require('../Db/connection');
 
-const FirstSchema = new mongoose.Schema(
-    {
-        name: {type: String},
-        img: {type: String},
-        title: {type: String},
-    },
-    
-  );
+const FirstSchema = new mongoose.Schema({
+    user:{
+        username: String,
+        profilePic: String,
+        password: String
+        },
+        
+        Marketing_info:{
+        gitHub:String,
+        linkedIn:String,
+        resume:String,
+        coverLetter:String
+        },
+        
+        reach_out_to_Dev:{
+        yourEmail:String,
+        yourName:String,
+        yourQuestion:String
+        },
+        
+        Projects:{
+
+           project:{
+                      image:String,
+                      shortVideo:String,
+                      Discription:String,
+                      
+                    },
+
+           updates:{
+                      updateDiscription:String,
+                      additions:String,
+                      updatedAt: {type: Date, default: Date.now}
+                      },
+        }
+    });
   
   const setFirstShema = mongoose.model('pro3schema', FirstSchema);
   
