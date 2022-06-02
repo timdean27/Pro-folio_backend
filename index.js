80fba01a6f1148e4c4eb37c4ea9d6f427bdabfe2
timdean27 <timdean27@yahoo.com>
2022-06-02 15:07:33 -0400

    removed port test for heroku

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(cors());
 app.use(express.json())
 app.use('/home', postRoutes)
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log("Express server listening on port", this.address().port, app.settings.env);
 });
 
