1c236776e24dfb0b7a83ffd7387372c870fbf0f5
timdean27 <timdean27@yahoo.com>
2022-06-02 15:10:40 -0400

    removed port test for heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const postRoutes = require('./Routes/routes.js')
 
 
-
+app.get('/favicon.ico', (req, res) => {res.send("dummy")})
 app.use(cors());
 app.use(express.json())
 app.use('/home', postRoutes)
